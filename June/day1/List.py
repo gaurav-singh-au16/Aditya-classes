4033413a9f7dc2d55328d177d5c428a1e9ebd1d5
Gaurav Singh <gauravsingh.info@gmail.com>
2021-06-16 18:39:00 +0530

    perivous class Material

---
+++ June/day1/List.py
@@ -0,0 +1,72 @@
+# Variable
+
+# name = "gaurav"
+# num = 12
+# studentName = "Aditya"
+# student_name = "Aditya"
+
+# string
+# num = "12" 
+# name = 155 
+# # typeCaste
+# print(str(name))
+# print(int(num))
+
+# print(type(str(name)))
+
+# for i in range(1, 15):
+#     print(i)
+
+# Data Structure
+
+# 1=> list
+# []
+# 2 => tuple
+# ("aditya")
+# 3 => dict
+# {} key:value
+# {name:"aditya", studentname:"aditya", item:[sas, ddg, fhff]}
+# 4 => set
+# set("aditya", "gaurav", "gaurav singh")
+
+#List []
+
+# 1 Append
+
+# a = [39, 100, 2, 35, 5, 8, 15, 900]
+# b = [200, 500, 600]
+# a.append(5)
+
+# 2 POP
+# a.pop()
+
+# 3 reverse
+# a.reverse()
+
+# 4 sort
+# a.sort()
+
+# 5 index
+# a.index(8)
+
+# 6 copy
+# c = a.copy()
+
+# 7 clear
+# a.clear()
+
+# 8 Extend
+
+# a.extend(b)
+
+# 9 remove
+# a.remove(900)
+
+# 10 insert
+# a.insert(2, 1200)
+
+# print(a)
+
+
+
+
---
+++ June/day2/slicing.py
@@ -0,0 +1,13 @@
+# Slicing in list
+
+a = [12, 33 , 77, 88, 99]
+b = "aditya"
+# finding length
+# print(len(a))
+# print(len(b))
+
+# c = a[0]
+
+# [:] => give the same list/string
+# [2:] => remove from front(balance)
+# [:2] => return the remove part
