0d30b4a3f7aa40e520132d5c42b79a050543a65a
Gaurav Singh <gauravsingh.info@gmail.com>
2021-06-23 17:23:51 +0530

    Matrix

---
+++ June/day3/matrix.py
@@ -0,0 +1,43 @@
+# Matrix
+# a = [[1,2,3],
+#     [4,5,6],
+#     [8,6,7],
+#     [11,22,33]]
+
+
+# sum
+# max
+# min
+
+# find the sum of given matrix?
+# print(len(a[0]))
+# print(len(a))
+
+# range=> n-1
+# 3-1=2
+# 0,1,2
+
+# tc O(n)
+# ans = 0
+# for i in range(len(a)):
+#     ans += sum(a[i]) 
+
+# print(ans)
+
+# o(n^2)
+# ans = 0
+# for i in range(len(a)): #3
+#     for j in range(len(a[0])): #2
+
+#         ans += a[i][j]  #
+
+# print(ans)
+
+
+# H.W => 
+# a = [[1,2,3],
+#     [4,5,6],
+#     [8,6,7]]
+
+# find out the sum of right diagnol & left diagnol?
+
